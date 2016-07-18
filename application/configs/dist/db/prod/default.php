0dd612e1c148a31c8cea65eb5222ce54f675f47b
Александр <shatalinalex@gmail.com>
2016-07-18 23:30:13 +0300

    install

--- application/configs/dist/db/prod/default.php
+++ application/configs/dist/db/prod/default.php
@@ -2,6 +2,7 @@
 return array(
 	'username' => 'hospital',
 	'password' => '3X5n9O3v3X5n9O3v',
+	'port'=> 3306,
 	'dbname'   => 'hospital',
 	'host'     => 'localhost',
 	'charset'  => 'UTF8',
--- application/configs/dist/db/prod/error.php
+++ application/configs/dist/db/prod/error.php
@@ -4,6 +4,7 @@ return array(
 	'password' => '3X5n9O3v3X5n9O3v',
 	'dbname'   => 'hospital',
 	'host'     => 'localhost',
+	'port'=> 3306,
 	'charset'  => 'UTF8',
 	'prefix'   => 'dv_',
 	'adapter'  => 'Mysqli',
